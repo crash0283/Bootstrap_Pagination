ac202f583163de4dd4c072c62c9850ee2f45a8bd
Chris Rasch <Chris Rasch>
2017-04-16 22:46:37 -0500

    added better layout, icons, colors, geo-based location

--- js/script.js
+++ js/script.js
@@ -1,5 +1,60 @@
 //Global Variables
 var curId;
+var myLat;
+var myLong;
+var city = 'Big Sky';
+var state = 'CO';
+var cloud = 'fa-cloud';
+var sun = 'fa-sun-o';
+var rain = 'fa-tint';
+var snow = 'fa-snowflake-o'
+
+
+window.onload = function () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(handleGetCurrentPosition, onError);
+    }
+}
+
+function handleGetCurrentPosition(location) {
+    myLat = location.coords.latitude;
+    myLong = location.coords.longitude;
+    
+    var weather = $.ajax(`http://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&units=imperial&appid=4f3823bf42f5cb17d953b5de50085cc1`).done(function () {
+        var temp = Math.round(weather.responseJSON.main.temp);
+        var desc = weather.responseJSON.weather[0].main;
+        console.log(desc)
+        if (desc == 'Clouds') {
+            $('i').addClass(cloud);
+            $('i').removeClass(sun).removeClass(rain).removeClass(snow);
+        }
+        else if (desc == 'Clear') {
+            $('i').removeClass(cloud).removeClass(snow).removeClass(rain);;
+            $('i').addClass(sun);
+        }
+        else if (desc == 'Rain') {
+            $('i').addClass(rain);
+            $('i').removeClass(cloud).removeClass(sun).removeClass(snow);
+        }
+        else if (desc == 'Snow') {
+            $('i').addClass(snow);
+            $('i').removeClass(cloud).removeClass(sun).removeClass(rain);
+        }
+        else {
+            $('i').addClass(sun);
+        }
+        $('.city').html(weather.responseJSON.name);
+        $('.lead').html(weather.responseJSON.weather[0].description);
+        $('.temp').html(temp + '&deg;F');
+        console.log(weather.responseJSON);
+    })
+}
+
+function onError() {
+    $('.city').addClass('text-danger').html('Location not avaliable!').css({fontSize: '24pt'});
+    $('.one').addClass('bg-danger');
+    $('i').css({display: 'none'});
+}
 $('#pressBlue').click(function () {
     $('#one').css({
         display: 'block'
@@ -28,9 +83,7 @@ $('#pressGreen').click(function () {
         addMe($(this));
     })
     //Set timer
-setInterval(() => {
-
-}, 2000);
+setInterval(() => {}, 2000);
 
 function addMe(head) {
     $('li').removeClass('active');
@@ -88,42 +141,4 @@ $('#previous').click(function () {
         curId = $('li.active a').attr('id');
         hitNext(curId);
     }
-})
-
-var city = 'Big Sky';
-var state = 'CO';
-var cloud = 'fa-cloud';
-var sun = 'fa-sun-o';
-var rain = 'fa-tint';
-var snow = 'fa-snowflake-o'
-
-var weather = $.ajax(`http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&units=imperial&appid=4f3823bf42f5cb17d953b5de50085cc1`)
-.done(function(){
-    var temp = Math.round(weather.responseJSON.main.temp);
-    var desc = weather.responseJSON.weather[0].main;
-    console.log(desc)
-    if(desc =='Clouds') {
-        $('i').addClass(cloud);
-        $('i').removeClass(sun).removeClass(rain).removeClass(snow);
-    }
-    else if(desc == 'Clear') {
-        $('i').removeClass(cloud).removeClass(snow).removeClass(rain);;
-        
-        $('i').addClass(sun);
-    }
-    else if(desc == 'Rain') {
-        $('i').addClass(rain);
-        $('i').removeClass(cloud).removeClass(sun).removeClass(snow);
-    }
-    else if(desc == 'Snow') {
-        $('i').addClass(snow);
-        $('i').removeClass(cloud).removeClass(sun).removeClass(rain);
-        
-    }
-    else {
-        $('i').addClass(sun);
-    }
-    
-    $('.city').html(city+','+state);
-    $('.temp').html(temp+'&deg;F');
 })
